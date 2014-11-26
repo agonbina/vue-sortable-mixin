0b7bb3d66c64515b3c2b35b32d05dddf5cb980d9
Agon Bina <agon_bina@hotmail.com>
2014-11-26 08:33:37 +0100

    first implementation

--- README.md
+++ README.md
@@ -2,3 +2,34 @@ vue-sortable-mixin
 ==================
 
 Keep in sync a list(array) of data in a Vue.js view model with jQueryUI sortable
+
+## Usage
+```bash
+ npm install --save vue-sortable-mixin
+```
+
+## Example
+
+```js
+var sortableMixin = require('vue-sortable-mixin');
+
+var MyComponent = Vue.extend({
+    mixins: [ sortableMixin ],
+
+    template: '<div v-repeat="positions"> {{$value}} </div>',
+
+    data: function() {
+        return {
+            positions: [ 'striker', 'defender', 'midfielder' ],
+            $sortable: {
+                key: 'positions' // Default is 'elements'
+            }
+        }
+    }
+})
+```
+
+Now the ```positions``` indexes will stay intact with any changes made by $.sortable.
+
+You can pass any of the regular $.fn.sortable options to ```$sortable``` in your data, including a
+special ```$el``` selector if your sortable DOM element is not the default viewModel.$el
---
+++ index.js
@@ -0,0 +1,37 @@
+
+module.exports = {
+    ready: function () {
+        var vm = this;
+        var options = this.$data.$sortable || {};
+        var $el = options.$el || this.$el;
+
+
+        var prevStart = options.start ? options.start : function() {};
+        options.start = function (event, ui) {
+            ui.item.data('start', ui.item.index());
+
+            // Call the user specified .start callback
+            prevStart(event, ui);
+        };
+
+        var prevUpdate = options.update ? options.update : function() {};
+        options.update = function (event, ui) {
+            var start = ui.item.data('start');
+            var end = ui.item.index();
+            var listKey = options.key ? options.key : 'elements';
+            var swappedEl = vm[listKey].splice(start, 1)[0];
+
+            // Move the item to the new position(index) and add back the swapped element
+            // which was holding that position
+            vm[listKey].splice(end, 0, swappedEl);
+
+            // Call the user specified .update callback
+            prevUpdate(event, ui);
+        };
+
+        // Remove the key from the $.fn.sortable options
+        if(options.key) delete options.key;
+
+        $($el).sortable(options);
+    }
+};
