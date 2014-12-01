bd983a53eb8a89a4aa2ad91d5a2f15cdba326517
Agon Bina <agon_bina@hotmail.com>
2014-12-01 19:25:57 +0100

    [bug] was setting the start and update callbacks on the same options object for every sortableMixin consumer

--- index.js
+++ index.js
@@ -1,39 +1,33 @@
 
 module.exports = {
-    ready: function () {
+    compiled: function () {
         var vm = this;
-        var options = this.$data.$sortable || {};
-        var $el = options.$el || this.$el;
+        var $el = this.$el;
+        var key = 'elements';
 
+        // Refresh jQuery sortable when we make changes to the listKey array
+        vm.$watch(key, function () {
+            $($el).sortable('refresh')
+        });
 
-        var prevStart = options.start ? options.start : function() {};
-        options.start = function (event, ui) {
+        var onStart = function (event, ui) {
             ui.item.data('start', ui.item.index());
-
-            // Call the user specified .start callback
-            prevStart(event, ui);
         };
 
-        var prevUpdate = options.update ? options.update : function() {};
-        options.update = function (event, ui) {
+        var onUpdate = function (event, ui) {
             var start = ui.item.data('start');
             var end = ui.item.index();
-            var listKey = options.key ? options.key : 'elements';
-            var swappedEl = vm[listKey].splice(start, 1)[0];
+            var array = vm.$get(key) || [];
+            var swappedEl = array.splice(start, 1)[0];
 
             // Move the item to the new position(index) and add back the swapped element
             // which was holding that position
-            vm[listKey].splice(end, 0, swappedEl);
-
-            // Refresh jQuery sortable when we make changes to the listKey array
-            vm.$watch(listKey, function () {
-                $($el).sortable('refresh')
-            });
-
-            // Call the user specified .update callback
-            prevUpdate(event, ui);
+            array.splice(end, 0, swappedEl);
         };
 
-        $($el).sortable(options);
+        $($el).sortable({
+            start: onStart,
+            update: onUpdate
+        });
     }
 };
